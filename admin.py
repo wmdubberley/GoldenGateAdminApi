9cf82762ee8e6b04f1f09fbc5978f8a7205916a4
William Dubberleywilliamdubberley@gmail.com <williamdubberley@gmail.com>
2023-04-07 14:38:23 -0500

    update

---
+++ admin.py
@@ -0,0 +1,73 @@
+from ggapi import GoldengateRestApiHelper
+import argparse
+import yaml
+
+def get_deployment_config(deployment_name):
+    with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+        for deployment in config['deployments']:
+            if deployment['deployment'] == deployment_name:
+                return deployment
+    return None
+
+def main():
+    parser = argparse.ArgumentParser(description='GoldenGate API CLI')
+    parser.add_argument('-d', '--deployment', type=str, required=True, help='Name of the deployment')
+    parser.add_argument('-e', '--extract_name', type=str, help='Name of the extract process')
+    parser.add_argument('-r', '--replicat_name', type=str, help='Name of the replicat process')
+    parser.add_argument('--Monitor-Deployment', action='store_true', help='Start an extract')
+    parser.add_argument('--start-extract', action='store_true', help='Start an extract')
+    parser.add_argument('--stop-extract', action='store_true', help='Stop an extract')
+    parser.add_argument('--start-replicat', action='store_true', help='Start a replicat')
+    parser.add_argument('--stop-replicat', action='store_true', help='Stop a replicat')
+    parser.add_argument('--list-extracts', action='store_true', help='List all extracts')
+    parser.add_argument('--list-replicats', action='store_true', help='List all replicats')
+    parser.add_argument('--extract-status', action='store_true', help='Get status of an extract')
+    parser.add_argument('--extract-details', action='store_true', help='Get details of an extract')
+    parser.add_argument('--replicat-status', action='store_true', help='Get status of a replicat')
+    parser.add_argument('--replicat-details', action='store_true', help='Get details of a replicat')
+    parser.add_argument('--view-extract-report', action='store_true', help='View extract report file')
+    parser.add_argument('--view-replicat-report', action='store_true', help='View replicat report file')
+    args = parser.parse_args()
+
+    deployment_config = get_deployment_config(args.deployment)
+    if not deployment_config:
+        print(f'Deployment "{args.deployment}" not found in config')
+        return
+
+    ggr = GoldengateRestApiHelper(deployment_config['baseURL'], deployment_config['username'],deployment_config['password']) 
+
+
+    if args.start_extract:
+        messages=ggr.start_extract(args.extract_name)['messages']
+        for mess in messages:
+            print(mess['title'])
+    elif args.stop_extract:
+        messages=ggr.stop_extract(args.extract_name)['messages']
+        for mess in messages:
+            print(mess['title'])
+    elif args.start_replicat:
+        messages=ggr.start_replicat(args.replicat_name)
+    elif args.stop_replicat:
+        messages=ggr.stop_replicat(args.replicat_name)
+    elif args.list_extracts:
+        messages=ggr.list_extracts()
+    elif args.list_replicats:
+        messages=ggr.list_replicats()
+    elif args.extract_status:
+        messages=ggr.get_extract_status(args.extract_name)
+    elif args.extract_details:
+        messages=ggr.get_extract_details(args.extract_name)
+    elif args.replicat_status:
+        messages=ggr.get_replicat_status(args.replicat_name)
+    elif args.replicat_details:
+        messages=ggr.get_replicat_details(args.replicat_name)
+    elif args.view_extract_report:
+        ggr.view_extract_report_file(args.extract_name)
+    elif args.view_replicat_report:
+        ggr.view_replicat_report_file(args.replicat_name)
+    else:
+        ggr.monitor_deployment()
+
+if __name__ == '__main__':
+    main()
--- ggapi.py
+++ ggapi.py
@@ -1,105 +1,261 @@
 import requests
-import yaml
 import datetime
+from tabulate import tabulate
+from requests.auth import HTTPBasicAuth
+from typing import List, Dict
 
-with open("config.yml", "r") as config_file:
-    config = yaml.safe_load(config_file)
+class GoldengateRestApiHelper():
+    """
+    A class for interacting with the GoldenGate API.
+    """
+ 
+    def __init__(self, base_url: str, username: str, password: str) -> None:
+        """
+        Initializes a new instance of the GoldenGateAPI class.
 
-username = config["username"]
-password = config["password"]
-base_url = config["base_url"]
+        Args:
+            base_url (str): The base URL of the GoldenGate API.
+            username (str): The username to use for authentication.
+            password (str): The password to use for authentication.
+        """
+        self.base_url = base_url
+        self.auth = HTTPBasicAuth(username, password)
 
+    def start_extract(self, extract_name: str) -> Dict:
+        """
+        Starts an extract.
 
-def list_extracts():
-    endpoint = f"{base_url}/extracts"
-    response = requests.get(endpoint, auth=(username, password))
-    return response.json()
+        Args:
+            extract_name (str): The name of the extract to start.
 
-def start_extract(extract_name):
-    endpoint = f"{base_url}/commands/execute"
-    data = {"name": "start", "processName": extract_name}
-    response = requests.post(endpoint, json=data, auth=(username, password))
-    return response.json()
+        Returns:
+            dict: A dictionary containing the response from the API.
+        """
+        endpoint = f"{self.base_url}/commands/execute"
+        data = {"name": "start", "processName": extract_name}
+        response = requests.post(endpoint, json=data, auth=self.auth)
+        return response.json()
 
-def stop_extract(extract_name):
-    endpoint = f"{base_url}/commands/execute"
-    data = {"name": "stop", "processName": extract_name}
-    response = requests.post(endpoint, json=data, auth=(username, password))
-    return response.json()
+    def stop_extract(self, extract_name: str) -> Dict:
+        """
+        Stops an extract.
 
-def get_extract_Stats(extract_name):
-    endpoint = f"{base_url}/mpoints/{extract_name}/statisticsExtract"
-    response = requests.get(endpoint, auth=(username, password))
-    return response.json()
+        Args:
+            extract_name (str): The name of the extract to stop.
 
-def get_extract_status(extract_name):
-    endpoint = f"{base_url}/extracts/{extract_name}/info/status"
-    response = requests.get(endpoint, auth=(username, password))
-    return response.json()
+        Returns:
+            dict: A dictionary containing the response from the API.
+        """
+        endpoint = f"{self.base_url}/commands/execute"
+        data = {"name": "stop", "processName": extract_name}
+        response = requests.post(endpoint, json=data, auth=self.auth)
+        return response.json()
 
-# Function to view extract report file
-def view_extract_report_file(extract_name):
-    url = f"{base_url}/extracts/{extract_name}/info/reports/{extract_name}.rpt"
-    response = requests.get(url, auth=(username, password), headers={"Content-Type": "application/json", "Accept": "application/json"})
-    response.raise_for_status()
-    now = datetime.datetime.now()
-    current_date = now.strftime("%Y-%m-%d")
-    with open(f"{extract_name}-{current_date}.rpt", "w") as f:
-        for i in response.json()['response']['lines']:
-            f.write(i)
-            f.write("\n")
-        # f.writelines(extract_report_file)
-    f.close()
+    def start_replicat(self, replicat_name: str) -> Dict:
+        """
+        Starts a replicat.
 
-def start_replicat(replicat_name):
-    endpoint = f"{base_url}/commands/execute"
-    data = {"name": "start", "processName": replicat_name}
-    response = requests.post(endpoint, json=data, auth=(username, password))
-    return response.json()
+        Args:
+            replicat_name (str): The name of the replicat to start.
 
-def stop_replicat(replicat_name):
-    endpoint = f"{base_url}/commands/execute"
-    data = {"name": "stop", "processName": replicat_name}
-    response = requests.post(endpoint, json=data, auth=(username, password))
-    return response.json()
+        Returns:
+            dict: A dictionary containing the response from the API.
+        """
+        endpoint = f"{self.base_url}/commands/execute"
+        data = {"name": "start", "processName": replicat_name}
+        response = requests.post(endpoint, json=data, auth=self.auth)
+        return response.json()
 
-def get_replicat_Stats(replicat_name):
-    endpoint = f"{base_url}/mpoints/{replicat_name}/statisticsReplicat"
-    response = requests.get(endpoint, auth=(username, password))
-    return response.json()
+    def stop_replicat(self, replicat_name: str) -> Dict:
+        """
+        Stops a replicat.
 
-def list_replicats():
-    endpoint = f"{base_url}/replicats"
-    response = requests.get(endpoint, auth=(username, password))
-    return response.json()
+        Args:
+            replicat_name (str): The name of the replicat to stop.
 
-def get_extract_details(extract_name):
-    url = f"{base_url}/extracts/{extract_name}"
-    response = requests.get(url, auth=(username, password), headers={"Content-Type": "application/json", "Accept": "application/json"})
-    response.raise_for_status()
-    return response.json()
+        Returns:
+            dict: A dictionary containing the response from the API.
+        """
+        endpoint = f"{self.base_url}/commands/execute"
+        data = {"name": "stop", "processName": replicat_name}
+        response = requests.post(endpoint, json=data, auth=self.auth)
+        return response.json()
 
-def get_replicat_status(replicat_name):
-    url = f"{base_url}/replicats/{replicat_name}/info/status"
-    response = requests.get(url, auth=(username, password), headers={"Content-Type": "application/json", "Accept": "application/json"})
-    response.raise_for_status()
-    return response.json()
+    def list_extracts(self) -> Dict:
+        """
+        Lists all extracts.
 
-def get_replicat_details(replicat_name):
-    url = f"{base_url}/replicats/{replicat_name}"
-    response = requests.get(url, auth=(username, password), headers={"Content-Type": "application/json", "Accept": "application/json"})
-    response.raise_for_status()
-    return response.json()
+        Returns:
+            dict: A dictionary containing the response from the API.
+        """
+        endpoint = f"{self.base_url}/extracts"
+        response = requests.get(endpoint, auth=self.auth)
+        return response.json()
 
-def view_replicat_report_file(replicat_name):
-    url = f"{base_url}/replicats/{replicat_name}/info/reports/{replicat_name}.rpt"
-    response = requests.get(url, auth=(username, password), headers={"Content-Type": "application/json", "Accept": "application/json"})
-    response.raise_for_status()
-    now = datetime.datetime.now()
-    current_date = now.strftime("%Y-%m-%d")
-    with open(f"{replicat_name}-{current_date}.rpt", "w") as f:
-        for i in response.json()['response']['lines']:
-            f.write(i)
-            f.write("\n")
-    f.close()
+    def list_replicats(self) -> Dict:
+        """
+        Lists all replicats.
+
+        Returns:
+            dict: A dictionary containing the response from the API.
+        """
+        endpoint = f"{self.base_url}/replicats"
+        response = requests.get(endpoint, auth=self.auth)
+        return response.json()
+
+    def get_extract_status(self, extract_name: str) -> Dict:
+        """
+        Gets the status of an extract.
+
+        Args:
+            extract_name (str): The name of the extract to get the status of.
+
+        Returns:
+            dict: A dictionary containing the response from the API.
+        """
+        endpoint = f"{self.base_url}/extracts/{extract_name}/info/status"
+        response = requests.get(endpoint, auth=self.auth)
+        return response.json()
+
+    def get_extract_details(self, extract_name: str) -> Dict:
+        """
+        Gets the details of a specific extract from the GoldenGate Deployment.
+        
+        Parameters:
+            extract_name (str): The name of the extract to get details for.
+        
+        Returns:
+            Dict: A dictionary containing the details of the specified extract.
+        """
+        url = f"{self.base_url}/extracts/{extract_name}"
+        response = requests.get(url, auth=self.auth, headers={"Content-Type": "application/json", "Accept": "application/json"})
+        response.raise_for_status()
+        return response.json()
+
+    def get_replicat_status(self, replicat_name: str) -> Dict:
+        """
+        Gets the status of a specific replicat from the GoldenGate Deployment.
+        
+        Parameters:
+            replicat_name (str): The name of the replicat to get status for.
+        
+        Returns:
+            Dict: A dictionary containing the status of the specified replicat.
+        """
+        url = f"{self.base_url}/replicats/{replicat_name}/info/status"
+        response = requests.get(url, auth=self.auth, headers={"Content-Type": "application/json", "Accept": "application/json"})
+        response.raise_for_status()
+        return response.json()
+
+    def get_replicat_details(self, replicat_name: str) -> Dict:
+        """
+        Gets the details of a specific replicat from the GoldenGate Deployment.
+        
+        Parameters:
+            replicat_name (str): The name of the replicat to get details for.
+        
+        Returns:
+            Dict: A dictionary containing the details of the specified replicat.
+        """
+        url = f"{self.base_url}/replicats/{replicat_name}"
+        response = requests.get(url, auth=self.auth, headers={"Content-Type": "application/json", "Accept": "application/json"})
+        response.raise_for_status()
+        return response.json()
+
+    # Function to view extract report file
+    def view_extract_report_file(self, extract_name: str) -> None:
+        """
+        Views the extract report file for a specific extract from the GoldenGate Deployment.
+        Writes the extract report to a file with the name <extract_name>-<current_date>.rpt
+        
+        Parameters:
+            extract_name (str): The name of the extract to get the report for.
+        """
+        url = f"{self.base_url}/extracts/{extract_name}/info/reports/{extract_name}.rpt"
+        response = requests.get(url, auth=self.auth, headers={"Content-Type": "application/json", "Accept": "application/json"})
+        response.raise_for_status()
+        now = datetime.datetime.now()
+        current_date = now.strftime("%Y-%m-%d")
+        with open(f"{extract_name}-{current_date}.rpt", "w") as f:
+            for i in response.json()['response']['lines']:
+                f.write(i)
+
+    # Function to view replicat report file
+    def view_replicat_report_file(self, replicat_name: str) -> None:
+        """
+        Downloads a replication report file from the given replicat name and saves it locally.
+        
+        Args:
+        - replicat_name: A string representing the name of the replicat whose report file needs to be downloaded.
+        
+        Returns:
+        - None
+        """
+        url = f"{self.base_url}/replicats/{replicat_name}/info/reports/{replicat_name}.rpt"
+        response = requests.get(url, auth=self.auth, headers={"Content-Type": "application/json", "Accept": "application/json"})
+        response.raise_for_status()
+        now = datetime.datetime.now()
+        current_date = now.strftime("%Y-%m-%d")
+        with open(f"{replicat_name}-{current_date}.rpt", "w") as f:
+            for i in response.json()['response']['lines']:
+                f.write(i)
+                f.write("\n")
+            # f.writelines(extract_report_file)
+        f.close()
+
+    def getStatus(self, typeOf: str, extract_Name: str) -> List:
+        """
+        Gets the status of an extract or a replicat and returns a list containing the status details.
+        
+        Args:
+        - typeOf: A string representing the type of object (extract or replicat).
+        - extract_Name: A string representing the name of the extract or replicat.
+        
+        Returns:
+        - A list containing the following items:
+            * typeOf: A string representing the type of object (extract or replicat).
+            * extract_Name: A string representing the name of the extract or replicat.
+            * status: A string representing the status of the extract or replicat.
+            * lastStarted: A string representing the date and time when the extract or replicat was last started.
+            * lag: An integer representing the lag time of the extract or replicat.
+            * mappedTotalInserts: An integer representing the total number of inserts in the extract or replicat.
+            * mappedTotalUpdates: An integer representing the total number of updates in the extract or replicat.
+            * mappedTotalDeletes: An integer representing the total number of deletes in the extract or replicat.
+        """
+        staticType='statisticsExtract'    
+        if typeOf == 'replicats':
+            staticType='statisticsReplicat' 
+        extract_status = requests.get(f'{self.base_url}/{typeOf}/\{extract_Name}/info/status',auth = self.auth).json()['response']
+        extract_stats = requests.get(f'{self.base_url}/mpoints/\{extract_Name}/{staticType}',auth = self.auth).json()['response']
+        return [typeOf,extract_Name,extract_status['status'],extract_status['lastStarted'],extract_status['lag'],extract_stats["mappedTotalInserts"],extract_stats["mappedTotalUpdates"],extract_stats["mappedTotalDeletes"]]
+
+    def getAllNames(self, typeOf: str) -> List[str]:
+        """
+        Retrieves a list of names for all extracts or replicats.
+
+        Args:
+            typeOf (str): The type of object to retrieve the names for (either 'extracts' or 'replicats').
+
+        Returns:
+            A list of strings representing the names of all extracts or replicats.
+        """
+        listOfExtracts = requests.get(f'{self.base_url}/{typeOf}',auth = self.auth).json()['response']
+        return listOfExtracts['items']
+
+    def monitor_deployment(self) -> None:
+        """
+        Prints a table displaying the status and statistics for all extracts and replicats.
+        
+        Returns:
+            None
+        """
+        ggTable = []
+        ggTable.append(['Type','Name','Status','Last Started','lag','Inserts','Updates','Deletes'])
+        for p in ['extracts','replicats']:
+            for item in self.getAllNames(p):
+                name=item['name']
+                ggTable.append(self.getStatus(p,name))
+
+        print(tabulate(ggTable, headers='firstrow', tablefmt='fancy_grid'))
+
 
